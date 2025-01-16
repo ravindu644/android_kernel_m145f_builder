0f2f44f32104664d48ecc6a6f7f75843fe723606
Ravindu Deshan <prasannakumarasinha26@gmail.com>
2025-01-16 17:24:52 +0000

    build script: make menuconfig support

--- build.sh
+++ build.sh
@@ -14,6 +14,8 @@ else
 fi
 cd "${RDIR}/kernel_platform"
 
-build/_setup_env.sh
-BUILD_CONFIG=msm-kernel/build.config.msm.m269.sec build/config.sh
-LTO=thin build/build.sh
+#kernel_platform/out/msm-kernel-m269-consolidate/gki_kernel/common/.config is the path for final config 
+#HERMETIC_TOOLCHAIN=0 is required for menuconfig to load external libraries
+
+HERMETIC_TOOLCHAIN=0 BUILD_CONFIG=msm-kernel/build.config.msm.m269.sec LTO=thin build/config.sh
+HERMETIC_TOOLCHAIN=0 BUILD_CONFIG=msm-kernel/build.config.msm.m269.sec LTO=thin build/build.sh
--- kernel_platform/build/kernel/build.sh
+++ kernel_platform/build/kernel/build.sh
@@ -854,6 +854,7 @@ echo "========================================================"
 echo " Building kernel"
 
 set -x
+(cd ${OUT_DIR} && make O=${OUT_DIR} ${TOOL_ARGS} menuconfig)
 (cd ${OUT_DIR} && make O=${OUT_DIR} ${TOOL_ARGS} "${MAKE_ARGS[@]}" ${MAKE_GOALS})
 set +x
 
